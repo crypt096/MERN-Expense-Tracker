ecd72f5143b2d7410a55e79e5577cce3c5080818
crypt096 <acvjetan@gmail.com>
2020-03-04 11:12:36 +0100

    Initial commit.

---
+++ routes/transactions.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getTransactions,
+  addTransaction,
+  deleteTransaction
+} = require("../controllers/transactions-controller");
+
+router
+  .route("/")
+  .get(getTransactions)
+  .post(addTransaction);
+
+router.route("/:id").delete(deleteTransaction);
+
+module.exports = router;
